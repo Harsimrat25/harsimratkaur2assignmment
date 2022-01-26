7fd7962367483e05d3a3e03be645ff5bc70ec0f6
Harsimrat25 <97719623+Harsimrat25@users.noreply.github.com>
2022-01-26 19:51:57 +0530

    Add files via upload

---
+++ assignment2output.py
@@ -0,0 +1,30 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 18:54:59) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\ACER\Desktop\assignment2qn.py ===============
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive language
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Hey,Harsimrat Kaur Here!
+My SID is 21104115
+I am from Electrical Engineering department and my CGPA is 9.9
+8
+58
+50
+224
+40
+14
+0
+Enter number in a: 23
+Enter number in b: 32
+Enter number in c: 3
+Largest number is: 32
+Enter a sentence:what a beautiful name
+Yes
+Length of 1st side:32
+Length of 2nd side:33
+Length of 3rd side:54
+Yes,Triangle can be formed
